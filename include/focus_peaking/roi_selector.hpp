db485bdf96a08109ad5c0014c0cb7afe62405bea
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-06-08 07:56:01 +0000

    separate the header and logic

--- include/focus_peaking/roi_selector.hpp
+++ include/focus_peaking/roi_selector.hpp
@@ -1,10 +1,13 @@
-#pragma once
+#ifndef FOCUS_PEAKING__ROI_SELECTOR_HPP_
+#define FOCUS_PEAKING__ROI_SELECTOR_HPP_
+
 #include <functional>
 #include <opencv2/opencv.hpp>
 #include <optional>
 #include <string>
-#include <thread>
 
+namespace focus_peaking
+{
 class ROISelector
 {
 private:
@@ -17,81 +20,26 @@ private:
   std::function<void(const cv::Rect &)> roi_callback_;
 
 public:
-  ROISelector(const std::string & win_name) : window_name_(win_name) {}
+  explicit ROISelector(const std::string & win_name);
 
-  void set_mouse_callback() { cv::setMouseCallback(window_name_, mouse_callback, this); }
+  void set_mouse_callback();
 
-  static void mouse_callback(int event, int x, int y, int flags, void * userdata)
-  {
-    ROISelector * selector = static_cast<ROISelector *>(userdata);
-    selector->handle_mouse(event, x, y, flags);
-  }
+  static void mouse_callback(int event, int x, int y, int flags, void * userdata);
 
-  void register_roi_callback(std::function<void(const cv::Rect &)> && callback)
-  {
-    roi_callback_ = std::move(callback);
-  }
+  void register_roi_callback(std::function<void(const cv::Rect &)> && callback);
 
-  void handle_mouse(int event, int x, int y, int /*flags*/)
-  {
-    switch (event) {
-      case cv::EVENT_LBUTTONDOWN:
-        drawing_ = true;
-        roi_complete_ = false;
-        start_point_ = cv::Point(x, y);
-        end_point_ = cv::Point(x, y);
-        break;
+  void handle_mouse(int event, int x, int y, int flags);
 
-      case cv::EVENT_MOUSEMOVE:
-        if (drawing_) {
-          end_point_ = cv::Point(x, y);
-        }
-        break;
+  void draw(cv::Mat & image);
 
-      case cv::EVENT_LBUTTONUP:
-        if (drawing_) {
-          drawing_ = false;
-          end_point_ = cv::Point(x, y);
+  bool is_roi_complete() const;
 
-          current_roi_ = cv::Rect(
-            std::min(start_point_.x, end_point_.x), std::min(start_point_.y, end_point_.y),
-            std::abs(end_point_.x - start_point_.x), std::abs(end_point_.y - start_point_.y));
+  std::optional<cv::Rect> get_roi() const;
 
-          if (current_roi_.width > min_roi_size_ && current_roi_.height > min_roi_size_) {
-            roi_complete_ = true;
-            if (roi_callback_) roi_callback_(current_roi_);
-          }
-        }
-        break;
-    }
-  }
+  void reset();
 
-  void draw(cv::Mat & image)
-  {
-    if (drawing_) {
-      cv::rectangle(image, start_point_, end_point_, cv::Scalar(255, 0, 0), 2);
-    } else if (!current_roi_.empty()) {
-      cv::rectangle(image, current_roi_, cv::Scalar(0, 255, 0), 2);
-    }
-  }
-
-  bool is_roi_complete() { return roi_complete_; }
-
-  std::optional<cv::Rect> get_roi()
-  {
-    if (!current_roi_.empty()) {
-      return current_roi_;
-    } else {
-      return std::nullopt;
-    }
-  }
-
-  void reset()
-  {
-    drawing_ = false;
-    roi_complete_ = false;
-    current_roi_ = cv::Rect();
-  }
-
-  void clear_roi_callback() { roi_callback_ = nullptr; }
+  void clear_roi_callback();
 };
+}  // namespace focus_peaking
+
+#endif  // FOCUS_PEAKING__ROI_SELECTOR_HPP_
