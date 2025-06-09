fccd434fa74c463d0580b1d1a360e4e4e32d29d5
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-06-09 05:47:07 +0000

    add launch parameters

--- launch/focus_peaking.launch.py
+++ launch/focus_peaking.launch.py
@@ -3,15 +3,20 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='focus_peaking',
-            executable='focus_peaking_node',
-            name='focus_peaking',
-            output='screen',
-            parameters=[
-                {"denoising_kernel_size": 3},
-                {"edge_dilation_kernel_size": 3},
-            ]
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="focus_peaking",
+                executable="focus_peaking_node",
+                name="focus_peaking",
+                output="screen",
+                parameters=[
+                    {"denoising_kernel_size": 3},  # Default: 3
+                    {"edge_dilation_kernel_size": 3},  # Default: 3
+                    {"widget_enabled": True},  # Default: True
+                    {"widget_width_ratio": 0.04},  # Default: 0.04
+                    {"focus_history_size": 200},  # Default: 200
+                ],
+            )
+        ]
+    )
--- launch/focus_peaking_debug.launch.py
+++ launch/focus_peaking_debug.launch.py
@@ -1,18 +1,23 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='focus_peaking',
-            executable='focus_peaking_node',
-            name='focus_peaking',
-            prefix=['xterm -fa Monospace -fs 14 -e gdb -ex run --args'],
-            output='screen',
-            parameters=[
-                {"denoising_kernel_size": 3},
-                {"edge_dilation_kernel_size": 3},
-            ]
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="focus_peaking",
+                executable="focus_peaking_node",
+                name="focus_peaking",
+                prefix=["xterm -fa Monospace -fs 14 -e gdb -ex run --args"],
+                output="screen",
+                parameters=[
+                    {"denoising_kernel_size": 3},  # Default: 3
+                    {"edge_dilation_kernel_size": 3},  # Default: 3
+                    {"widget_enabled": True},  # Default: True
+                    {"widget_width_ratio": 0.04},  # Default: 0.04
+                    {"focus_history_size": 200},  # Default: 200
+                ],
+            )
+        ]
+    )
